dce6fc0e31c9418bdb30d33d11fe018886953940
przemwo <przemwo@o2.pl>
2018-05-20 18:41:13 +0200

    refactored to use only react component

--- src/index.js
+++ src/index.js
@@ -3,91 +3,111 @@ import ReactDOM from 'react-dom';
 
 import './styles.css';
 
-const IFrame = () => {
-    return(
-        <iframe id="player" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media" title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/eIho2S0ZahI?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A8080&amp;widgetid=1"></iframe>
-    );
+class YouTubePlayer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            startAt: 14,
+            stopAt: 18,
+            done: false
+        };
+    }
+    onYouTubeIframeAPIReady = () => {
+        this.player = new YT.Player('player', {
+            events: {
+                'onReady': this.onPlayerReady,
+                'onStateChange': this.onPlayerStateChange,
+            }
+        });
+    }
+    onPlayerReady = (event) => {}
+    onPlayerStateChange = (event) => {
+        if (event.data == YT.PlayerState.PLAYING && !this.done) {
+            const { startAt, stopAt } = this.state;
+            const playDurationInSeconds = (startAt > stopAt) ? 0 : (stopAt - startAt);
+            this.timeOutId = setTimeout(this.pauseVideo, playDurationInSeconds * 1000);
+            this.setState({ done: true });
+        }
+    }
+    handleOnClickPlay = (event) => {
+        this.playVideo(event);
+    }
+    playVideo = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({ done: false });
+        clearTimeout(this.timeOutId);
+        this.player.seekTo(this.state.startAt);
+        this.player.playVideo();
+    };
+    pauseVideo = () => {
+        this.player.pauseVideo();
+    }
+    handleOnInput = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const name = event.target.name;
+        const value = parseFloat(event.target.value);
+        this.setState((state) => ({
+            [name]: isNaN(value) ? state[name] : value
+        }));
+    };
+    componentDidMount() {
+        window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady;
+        // Keybaord events
+        document.addEventListener('keyup', (event) => {
+            const keyName = event.key;
+            if (keyName === 'p') {
+                this.playVideo(event);
+            }
+        });
+    }
+    render() {
+        const { startAt, stopAt } = this.state;
+        return(
+            <div className="container">
+                <h1>Learn any language Tube</h1>
+                <iframe id="player" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media" title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/eIho2S0ZahI?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A8080&amp;widgetid=1"></iframe>
+                <div>
+                    <label for="startAt">Start at (in seconds):</label>
+                    <input
+                        id="startAt"
+                        name="startAt"
+                        type="text"
+                        value={startAt}
+                        onChange={this.handleOnInput}
+                    />
+                </div>
+                <div>
+                    <label for="stopaAt">Stop at (in seconds):</label>
+                    <input
+                        id="stopAt"
+                        name="stopAt"
+                        type="text"
+                        value={stopAt}
+                        onChange={this.handleOnInput}
+                    />
+                </div>
+                <button
+                    onClick={this.handleOnClickPlay}
+                >
+                    Play (P)
+                </button>
+            </div>
+        );
+    }
 };
 
 ReactDOM.render(
     <div>
-        <IFrame />
+        <YouTubePlayer />
     </div>,
     document.getElementById('app')
 );
 
+// TODO:
+// Load this script dynamically
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 const  firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// APP
-const state = {
-    startAt: 14,
-    stopAt: 18
-};
-const $playButton = document.getElementById('playButton');
-const $startAt = document.getElementById('startAt');
-const $stopAt = document.getElementById('stopAt');
-let player;
-let done = false;
-let timeOutId;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-window.onYouTubePlayerAPIReady = onYouTubePlayerAPIReady;
-
-function onYouTubePlayerAPIReady() {}
-
-
-function onPlayerReady(event) {}
-
-function pauseVideo() {
-    player.pauseVideo();
-}
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        const { startAt, stopAt } = state;
-        const playDurationInSeconds = (startAt > stopAt)? 0 : (stopAt - startAt);
-        timeOutId = setTimeout(pauseVideo,  playDurationInSeconds * 1000);
-        done = true;
-    }
-}
-
-
-const playVideo = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    done = false;
-    clearTimeout(timeOutId);
-    player.seekTo(state.startAt);
-    player.playVideo();
-};
-$playButton.addEventListener('click', playVideo);
-
-
-
-const handleOnInput = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    const name = event.target.name;
-    const value = parseFloat(event.target.value);
-    state[name] = isNaN(value) ? state[name] : value;    
-};
-$startAt.addEventListener('input', handleOnInput);
-$stopAt.addEventListener('input', handleOnInput);
-
-// Keybaord events
-document.addEventListener('keyup', (event) => {
-    const keyName = event.key;
-    if(keyName === 'p') {
-        playVideo(event);
-    }
-});
